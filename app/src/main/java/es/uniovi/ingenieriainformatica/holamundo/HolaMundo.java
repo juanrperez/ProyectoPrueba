06abe8449a5cc1ce53a28a6df35fa1f7ad45b5ab
juanrperez <jrpp@uniovi.es>
2017-02-08 13:24:35 +0100

    Primera subida de todos los ficheros.

---
+++ app/src/main/java/es/uniovi/ingenieriainformatica/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package es.uniovi.ingenieriainformatica.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HolaMundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo);
+    }
+}
